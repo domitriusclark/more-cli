ac655f891ff6fd1f23d2b58bbc7cc3c07272a5ea
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-27 22:59:21 -0400

    another one

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 const fs = require("fs")
 
-fs.readdir(__dirname + '/templates', (err, dirs) => {
+fs.readdir('../../../templates/', (err, dirs) => {
   if (err) {
     console.log(err)
   } else {
