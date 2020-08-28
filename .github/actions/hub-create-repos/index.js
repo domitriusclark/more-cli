726f2acec59c9ec3e00a250d9d0578fe1215ad31
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-28 00:42:02 -0400

    wow

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -5,27 +5,22 @@ const util = require('util');
 const readdir = util.promisify(fs.readdir);
 
 async function run() {
-  try {
-    await readdir('./templates', (err, dirs) => {
-      if (err) {
-        console.log(err)
-      } else {
-        for (let dir of dirs) {
-          exec(`hub create domitriusclark/${dir}`, (error, stdout, stderr) => {
-            if (error) {
-              console.error(`exec error: ${error}`);
-              return;
-            }
-            console.error(`stderr: ${stderr}`);
-            console.log(`stdout: ${stdout}`);
-          });
-        }
+  await readdir('./templates', (err, dirs) => {
+    if (err) {
+      console.log(err)
+    } else {
+      for (let dir of dirs) {
+        exec(`hub create domitriusclark/${dir}`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.error(`stderr: ${stderr}`);
+          console.log(`stdout: ${stdout}`);
+        });
       }
-    })
-  } catch (error) {
-    console.log(error.message)
-  }
-
+    }
+  })
 }
 
 run()
