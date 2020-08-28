cf98684dbf868c6073bd6a3215fbc832f4fe33c5
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-28 12:05:20 -0400

    Ben wants some logs

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -7,6 +7,7 @@ const readdir = util.promisify(fs.readdir);
 async function run() {
   try {
     const dirs = await readdir(process.env.GITHUB_WORKSPACE + '/templates')
+    console.log(dirs);
     for (let dir of dirs) {
       exec(`gh repo create ${dir}`)
     }
