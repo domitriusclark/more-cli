d39ecc24335a6fee02a666cf1fd7ffeedfb202d1
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-28 00:06:57 -0400

    one more try w/ jacob brain

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -1,24 +1,26 @@
 const { exec } = require('child_process');
 const fs = require("fs")
-const path = require("path");
 
-const directoryPath = path.join(__dirname, '../../../templates');
-
-fs.readdir(directoryPath, (err, dirs) => {
-  if (err) {
-    console.log(err)
-  } else {
-    for (let dir of dirs) {
-      exec(`hub create domitriusclark/${dir}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-        console.error(`stderr: ${stderr}`);
-        console.log(`stdout: ${stdout}`);
-      });
+function run() {
+  await fs.readdir('./templates', (err, dirs) => {
+    if (err) {
+      console.log(err)
+    } else {
+      for (let dir of dirs) {
+        exec(`hub create domitriusclark/${dir}`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.error(`stderr: ${stderr}`);
+          console.log(`stdout: ${stdout}`);
+        });
+      }
     }
-  }
-})
+  })
 
 
+
+}
+
+run()
