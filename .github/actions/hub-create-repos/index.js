c3ce478517e2e7e3f3a108f78e21d8094324a173
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-28 12:07:14 -0400

    Hoping for the best

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 async function run() {
   try {
-    const dirs = fs.readdir(process.env.GITHUB_WORKSPACE + '/templates')
+    const dirs = fs.readdirSync(process.env.GITHUB_WORKSPACE + '/templates')
     console.log(dirs);
     for (let dir of dirs) {
       exec(`gh repo create ${dir}`)
