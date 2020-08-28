98fd6e2891b701958957c298bc24aa8967fa4bbe
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-28 00:19:10 -0400

    plsworkibegu

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -1,26 +1,27 @@
 const { exec } = require('child_process');
 const fs = require("fs")
 
-function run() {
-  fs.readdir('./templates', (err, dirs) => {
-    if (err) {
-      console.log(err)
-    } else {
-      for (let dir of dirs) {
-        exec(`hub create domitriusclark/${dir}`, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          console.error(`stderr: ${stderr}`);
-          console.log(`stdout: ${stdout}`);
-        });
+async function run() {
+  try {
+    await fs.readdir('./templates', (err, dirs) => {
+      if (err) {
+        console.log(err)
+      } else {
+        for (let dir of dirs) {
+          exec(`hub create domitriusclark/${dir}`, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              return;
+            }
+            console.error(`stderr: ${stderr}`);
+            console.log(`stdout: ${stdout}`);
+          });
+        }
       }
-    }
-  })
-
-
-
+    })
+  } catch (e) {
+    console.log(e.message)
+  }
 }
 
 run()
