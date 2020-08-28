351810cf67623cc4138217e2cf5e6fe1d41d8eee
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-28 12:07:04 -0400

    Tony's liking it

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -1,12 +1,9 @@
 const { exec } = require('child_process');
 const fs = require("fs")
-const util = require('util');
-
-const readdir = util.promisify(fs.readdir);
 
 async function run() {
   try {
-    const dirs = await readdir(process.env.GITHUB_WORKSPACE + '/templates')
+    const dirs = fs.readdir(process.env.GITHUB_WORKSPACE + '/templates')
     console.log(dirs);
     for (let dir of dirs) {
       exec(`gh repo create ${dir}`)
