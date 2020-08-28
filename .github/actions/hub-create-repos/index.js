00ba0893c609bc200143857a44e1990172ed25e0
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-28 11:17:10 -0400

    using gh cli

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -1,32 +1,23 @@
 const { exec } = require('child_process');
 const fs = require("fs")
-const util = require('util');
 
-const readdir = util.promisify(fs.readdir);
-
-async function run() {
-  try {
-    await readdir('./templates', (err, dirs) => {
-      console.log(dirs)
-      if (err) {
-        console.log(err)
-      } else {
-        for (let dir of dirs) {
-          exec(`hub create domitriusclark/${dir}`, (error, stdout, stderr) => {
-            if (error) {
-              console.error(`exec error: ${error}`);
-              return;
-            }
-            console.error(`stderr: ${stderr}`);
-            console.log(`stdout: ${stdout}`);
-          });
-        }
+function run() {
+  return fs.readdir('./templates', (err, dirs) => {
+    if (err) {
+      console.log(err)
+    } else {
+      for (let dir of dirs) {
+        exec(`gh repo create domitriusclark/${dir}`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.error(`stderr: ${stderr}`);
+          console.log(`stdout: ${stdout}`);
+        });
       }
-    })
-  } catch (error) {
-    console.log(error.message)
-  }
-
+    }
+  })
 }
 
 run()
